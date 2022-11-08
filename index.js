9dad9b5ccdf2fdb0f86162c5454c25af5b9993ba
Admin <Admin@USERTL6.tpsonline.com>
2022-11-08 12:58:07 +0500

    Create API

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-const { response } = require('express')
+const { response, request } = require('express')
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 const pool = require('./dbConn')
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World')
-// })
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
 
 app.get('/testdb', async (request, response) => {
   let res =  await pool.query('select * from public.todoList')
   console.log(res)
-  response.json({info: 'Node.js, Express, and Postgres API'})
+  //response.json({info: 'Node.js, Express, and Postgres API'})
+  response.json({todo: res.rows})
+})
+
+app.post('/todo/create', async (req, res) =>{
+  let result = await pool.query('INSERT INTO public.todoList(id, task) VALUES($1, $2)', [req.body.id, req.body.task])
+  console.log(result)
+  res.json({
+    "status": "Task created"
+  })
+})
+
+app.put('/todo/update', async (req,res) =>{
+  let result = await pool.query('UPDATE public.todoList(id, task) WHERE(id = 4)', [req.body.id, req.body.task])
+  console.log(result);
+  res.json({
+    "status": "Task Updated"
+  })
 })
     
 
